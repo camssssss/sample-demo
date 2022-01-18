32c1ff1ce01a4470bf1e37d41057363096b3f100
camssssss <91815215+camssssss@users.noreply.github.com>
2022-01-18 22:46:05 +0800

    Create sampol.php

---
+++ sampol.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Camille's portfolio</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <?php
+        include('nav.php');
+    ?>
+    <header class="bg-light text-center">
+        <div class="container container--narrow">
+            <p>Hi! my name is Camille Cabrieto</p>
+            <h1>Welcome to Camille's Portfolio</h1>
+        </div>
+    </header>
+
+    <section>
+        <div class="container">
+            <h2 class="text-center">Quality Design</h2>
+            <div class="split">
+                <div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi nostrum temporibus quis id exercitationem corrupti sed similique est accusamus doloremque. Voluptatibus excepturi sit cum! Fuga!</p>
+                </div>
+                <div>
+                    <p>Odit esse, voluptate ratione dolore totam ipsa eaque iusto neque rerum corrupti aspernatur quia, praesentium numquam distinctio at deleniti rem, sint magni! Voluptatibus, deserunt dolor.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="bg-dark">
+        <div class="container">
+            <div class="split">
+                <div>
+                    <h2>Made Custom for you</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum libero dolor culpa id nisi iure!</p>
+                </div>
+                <div>
+                    <img src="sunset.jfif" alt="Smile">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="bg-primary">
+        <div class="container">
+            <div class="split">
+                <div>
+                    <img src="leaf.jpg" alt="Sad">
+                </div>
+                <div>
+                    <h2>Created with care</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam sint distinctio rerum cumque? Voluptas, libero? In praesentium quis culpa vero.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</body>
+</html>
